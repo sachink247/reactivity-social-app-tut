4bd2ede61f4f7378d1734366b145398fdd83dccd
sachink247 <sachink247@gmail.com>
2025-06-10 18:57:39 +0530

    end of sec 3

--- API/Program.cs
+++ API/Program.cs
@@ -4,7 +4,6 @@ using Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(
     opt =>
@@ -12,10 +11,14 @@ builder.Services.AddDbContext<AppDbContext>(
         opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
     }
 );
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().
+WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
