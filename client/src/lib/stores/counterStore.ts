4c3cb596a50d219b3625f5d32b2ec34be9ba1bb8
sachink247 <sachink247@gmail.com>
2025-06-23 14:42:44 +0530

    end of section 9

---
+++ client/src/lib/stores/counterStore.ts
@@ -0,0 +1,31 @@
+import { makeAutoObservable} from 'mobx'
+
+export default class CounterStore{
+    title = 'Counter Store';
+    count = 42;
+    events: string [] =[
+        `Initial Count is ${this.count}`
+    ]
+
+    constructor(){
+        makeAutoObservable(this)
+    }
+
+    increment=(amount =1)=>{ // this convention is required
+        this.count+=amount;
+        this.events.push(
+            `Incremented by ${amount} - count is now ${this.count}`
+        );
+    }
+
+    decrement=(amount=1)=>{
+        this.count-=amount;
+        this.events.push(
+            `Decremented by ${amount} - count is now ${this.count}`
+        );
+    }
+
+    get eventCount(){
+        return this.events.length
+    }
+}
